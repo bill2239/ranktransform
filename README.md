7f164b4d97bafd7664d21e3d2e616c3cab27e77e
lichi huang <bill2239@gmail.com>
2018-10-05 17:11:04 -0500

    add census transform

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # ranktransform
-perform rank transform to image, make it invariant to illumination changes
+perform rank transform or census transform to image, make it invariant to illumination changes, it is interesting to actually visualize how transform does  
+rank transform: adding up the number of neighbors that larger than central pixel  
+census transform : assigning neighbors that larger than central pixel to 1, 0 otherwise, and aggregate bits to value   
+
 To run this code   
 ```bash
-python ranktransform.py input_imgpath output_imgpath
+python rank_transform.py input_imgpath output_imgpath transform_type
 ```
 original image:  
 ![alt text](im1.png)
 rank transform:  
-![alt text](rt1.png)
+![alt text](rt1.png)  
+census transform:  
+![alt text](census.png)
--- rank_transform.py
+++ rank_transform.py
@@ -28,7 +28,41 @@ def rank_transform(img_path,windowsize):
     rt = 255.0 * ( rt - minVal ) / (maxVal - minVal - 1.0)  
     return rt
 
-rt= rank_transform(sys.argv[2],3)
+def census_transform(img_path,windowsize):
+    image = cv2.imread(img_path, 0)
+    height = len(image)
+    width = len(image[0])
+     
+    #window size
+    hy = windowsize
+    wx = windowsize
+     
+    for y in xrange(hy/2, height - hy/2):
+        for x in xrange(wx/2, width - wx/2):
+            count = 0
+            #MxN
+            for j in xrange(y - hy/2, y + hy/2 + 1):
+                for i in xrange(x - wx/2, x + wx/2 + 1):
+                    count>>1
+                    if image[j][i] > image[y][x]:
+                        count=count | 1
+                    else:
+                        count=count | 0
+            image[y][x] = count
+    rt= image[1:height-1,1:width-1] #offet by 1
+    #optional -- scale to unsigned 8 bit for visualization 
+    minVal=0
+    maxVal= 2 ** (windowsize*windowsize-1)
+    rt = 255.0 * ( rt - minVal ) / (maxVal - minVal - 1.0)  
+    return rt
+
+#transform_type= "census"
+transform_type= sys.argv[3]
+
+if transform_type == "rank":
+    rt= rank_transform(sys.argv[1],3)
+else:
+    rt=rank_transform(sys.argv[1],3)
 cv2.imshow("rank", rt)
 cv2.waitKey(0)
 
